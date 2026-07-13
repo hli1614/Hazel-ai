e8d08b58858b23de11a0a3f16795ce276266347f
hli1614 <hli1614@sdsu.edu>
2026-07-13 15:05:50 -0700

    Update hazel_pytorch_v1.py

--- hazel_pytorch_v1.py
+++ hazel_pytorch_v1.py
@@ -1,18 +1,45 @@
 import torch
-x = torch.tensor([1,2,3])
-print(x)
-print(type(x))
-print(x.shape)
-print(x * 2)
+import torch.nn as nn
 
-A = torch.tensor([
-    [1,2,3],
-    [4,5,6]
+#x = torch.tensor([1, 2, 3])
+
+# W = torch.tensor([
+#     [1,2,3],
+#     [4,5,6]
+# ])
+
+#B = torch.tensor([
+   ##@[3]
+#])
+
+#print(A @ B)
+
+#print(x)
+
+#print(type(x))
+
+#print(x.shape)
+
+layer = nn.Linear(3,2)
+
+x = torch.tensor([
+    [1.0,2.0,3.0]
 ])
-B = torch.tensor([
-    [1],
-    [2],
-    [3]
-])
-print(A @ B)
-print((A @ B).shape)
+print()
+
+print("Input =")
+print(x)
+
+print()
+
+print("Output =")
+print(layer(x))
+print()
+
+print(layer.weight)
+print(layer.weight.shape)
+print()
+
+print(layer.bias)
+print(layer.bias.shape)
+print()
